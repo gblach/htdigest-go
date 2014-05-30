a2d05685a70377b7da39432370efa3a012d66542
Sheng Yu <yusheng.sjtu@gmail.com>
2014-05-30 15:44:18 -0700

    Add windows support

--- passwd.go
+++ passwd.go
@@ -1,45 +1,16 @@
 package main
 
-// #include <termios.h>
-// #include <unistd.h>
-// struct termios T_orig, T;
-import "C"
-
-import (
-    "fmt"
-    "os"
-)
-
-
 func read_password() string {
-    print("Password: ")
-    passwd := stdin_read_password()
+	print("Password: ")
+	passwd := stdin_read_password()
 
-    print("Again: ")
-    pwd_again := stdin_read_password()
+	print("Again: ")
+	pwd_again := stdin_read_password()
 
-    if(passwd != pwd_again) {
-        panic("Passwords don't match")
-    }
+	print(len(passwd))
+	if passwd != pwd_again {
+		panic("Passwords don't match")
+	}
 
-    return passwd
-}
-
-func stdin_read_password() string {
-    stdin := C.int(os.Stdin.Fd())
-
-    C.tcgetattr(stdin, &C.T)
-    C.T_orig = C.T
-
-    C.T.c_lflag &^= C.ECHO
-    C.T.c_lflag |= C.ECHONL
-
-    C.tcsetattr(stdin, C.TCSANOW, &C.T)
-
-    var passwd string
-    fmt.Scanf("%s", &passwd)
-
-    C.tcsetattr(stdin, C.TCSANOW, &C.T_orig)
-
-    return passwd
+	return passwd
 }
