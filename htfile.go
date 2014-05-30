8b7e528c83be2f0fa5f70e26b88edbca18314bff
Sheng Yu <yusheng.sjtu@gmail.com>
2014-05-30 15:44:57 -0700

    Run go fmt

--- htfile.go
+++ htfile.go
@@ -1,68 +1,66 @@
 package main
 
 import (
-    "bufio"
-    "crypto/md5"
-    "fmt"
-    "io"
-    "os"
-    "strings"
-    "syscall"
+	"bufio"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"syscall"
 )
 
-
-var htdata = map[string] string{}
-
+var htdata = map[string]string{}
 
 func add_or_change_user(realm string, user string) {
-    passwd := read_password()
+	passwd := read_password()
 
-    hash := md5.New()
-    io.WriteString(hash, fmt.Sprintf("%s:%s:%s", user, realm, passwd))
+	hash := md5.New()
+	io.WriteString(hash, fmt.Sprintf("%s:%s:%s", user, realm, passwd))
 
-    key := fmt.Sprintf("%s:%s", user, realm)
-    val := fmt.Sprintf("%x", hash.Sum(nil))
-    htdata[key] = val
+	key := fmt.Sprintf("%s:%s", user, realm)
+	val := fmt.Sprintf("%x", hash.Sum(nil))
+	htdata[key] = val
 }
 
 func delete_user(realm string, user string) {
-    key := fmt.Sprintf("%s:%s", user, realm)
-    delete(htdata, key)
+	key := fmt.Sprintf("%s:%s", user, realm)
+	delete(htdata, key)
 }
 
 func load_htfile(htfile string) {
-    fh, err := os.Open(htfile)
-    if err != nil && err.(*os.PathError).Err == syscall.ENOENT {
-        return
-    } else if err != nil {
-        panic(err)
-    }
-    defer fh.Close()
+	fh, err := os.Open(htfile)
+	if err != nil && err.(*os.PathError).Err == syscall.ENOENT {
+		return
+	} else if err != nil {
+		panic(err)
+	}
+	defer fh.Close()
 
-    reader := bufio.NewReader(fh)
-    for {
-        line, _, err := reader.ReadLine()
+	reader := bufio.NewReader(fh)
+	for {
+		line, _, err := reader.ReadLine()
 
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            panic(err)
-        }
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
 
-        elm := strings.Split(string(line), ":")
-        key := fmt.Sprintf("%s:%s", elm[0], elm[1])
-        htdata[ key ] = elm[2]
-    }
+		elm := strings.Split(string(line), ":")
+		key := fmt.Sprintf("%s:%s", elm[0], elm[1])
+		htdata[key] = elm[2]
+	}
 }
 
 func save_htfile(htfile string) {
-    fh, err := os.Create(htfile)
-    if err != nil {
-        panic(err)
-    }
-    defer fh.Close()
+	fh, err := os.Create(htfile)
+	if err != nil {
+		panic(err)
+	}
+	defer fh.Close()
 
-    for key, val := range htdata {
-        fmt.Fprintf(fh, "%s:%s\n", key, val)
-    }
+	for key, val := range htdata {
+		fmt.Fprintf(fh, "%s:%s\n", key, val)
+	}
 }
--- main.go
+++ main.go
@@ -1,47 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
-
 func catch() {
-    if err := recover(); err != nil {
-        fmt.Fprintf(os.Stderr, "%s failed: %s\n", os.Args[0], err)
-        os.Exit(1)
-    }
+	if err := recover(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s failed: %s\n", os.Args[0], err)
+		os.Exit(1)
+	}
 }
 
 func usage() {
-    fmt.Printf("Usage: %s <htfile> <add|del> <realm> <user>\n", os.Args[0])
-    os.Exit(0)
+	fmt.Printf("Usage: %s <htfile> <add|del> <realm> <user>\n", os.Args[0])
+	os.Exit(0)
 }
 
 func main() {
-    defer catch()
+	defer catch()
 
-    if 5 != len(os.Args) {
-        usage()
-    }
+	if 5 != len(os.Args) {
+		usage()
+	}
 
-    htfile := os.Args[1]
-    action := os.Args[2]
-    realm := os.Args[3]
-    user := os.Args[4]
+	htfile := os.Args[1]
+	action := os.Args[2]
+	realm := os.Args[3]
+	user := os.Args[4]
 
-    switch action {
-        case "add":
-            load_htfile(htfile)
-            add_or_change_user(realm, user)
-            save_htfile(htfile)
+	switch action {
+	case "add":
+		load_htfile(htfile)
+		add_or_change_user(realm, user)
+		save_htfile(htfile)
 
-        case "del":
-            load_htfile(htfile)
-            delete_user(realm, user)
-            save_htfile(htfile)
+	case "del":
+		load_htfile(htfile)
+		delete_user(realm, user)
+		save_htfile(htfile)
 
-        default:
-            usage()
-    }
+	default:
+		usage()
+	}
 }
