83faa66dfe0e085acebe15ad54ad257907b73aee
Grzegorz Blach <grzegorz@blach.pl>
2014-06-18 14:40:17 +0200

    Add windows support

--- passwd.go
+++ passwd.go
@@ -1,16 +1,5 @@
 package main
 
-// #include <termios.h>
-// #include <unistd.h>
-// struct termios T_orig, T;
-import "C"
-
-import (
-    "fmt"
-    "os"
-)
-
-
 func read_password() string {
     print("Password: ")
     passwd := stdin_read_password()
@@ -21,25 +10,6 @@ func read_password() string {
     if(passwd != pwd_again) {
         panic("Passwords don't match")
     }
-
-    return passwd
-}
-
-func stdin_read_password() string {
-    stdin := C.int(os.Stdin.Fd())
-
-    C.tcgetattr(stdin, &C.T)
-    C.T_orig = C.T
-
-    C.T.c_lflag &^= C.ECHO
-    C.T.c_lflag |= C.ECHONL
-
-    C.tcsetattr(stdin, C.TCSANOW, &C.T)
-
-    var passwd string
-    fmt.Scanf("%s", &passwd)
-
-    C.tcsetattr(stdin, C.TCSANOW, &C.T_orig)
 
     return passwd
 }
---
+++ passwd_unix.go
@@ -0,0 +1,31 @@
+package main
+
+// #include <termios.h>
+// #include <unistd.h>
+// struct termios T_orig, T;
+import "C"
+
+import (
+    "fmt"
+    "os"
+)
+
+
+func stdin_read_password() string {
+    stdin := C.int(os.Stdin.Fd())
+
+    C.tcgetattr(stdin, &C.T)
+    C.T_orig = C.T
+
+    C.T.c_lflag &^= C.ECHO
+    C.T.c_lflag |= C.ECHONL
+
+    C.tcsetattr(stdin, C.TCSANOW, &C.T)
+
+    var passwd string
+    fmt.Scanf("%s", &passwd)
+
+    C.tcsetattr(stdin, C.TCSANOW, &C.T_orig)
+
+    return passwd
+}
